0b9f02c5abf125ebd14a72a07e21eb63f5463411
hbilu <hacerbilu@gmail.com>
2023-06-05 15:27:11 +0200

    update
    Implement Stack using Linked List

---
+++ Implement_Stack_using_Linked_List.py
@@ -0,0 +1,40 @@
+"""
+Your Task:
+ are required to complete two methods push() and pop(). T
+ he push() method takes one argument, an integer 'x' to be pushed into the stack and
+ pop() which returns an integer present at the top and popped out from the stack.
+ If the stack is empty then return -1 from the pop() method.
+
+Expected Time Complexity: O(1) for both push() and pop().
+Expected Auxiliary Space: O(1) for both push() and pop()
+
+"""
+
+class StackNode:
+    # Constructor to initialize a node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class MyStack:
+
+    def __init__(self):
+        self.head = None
+
+    # Function to push an integer into the stack.
+    def push(self, data):
+        if self.head == None:
+            self.head = StackNode(data)
+        else:
+            newnode = StackNode(data)
+            newnode.next = self.head
+            self.head = newnode
+
+    # Function to remove an item from top of the stack.
+    def pop(self):
+        if self.head == None:
+            return -1
+        poppednode = self.head
+        self.head = self.head.next
+        poppednode.next = None
+        return poppednode.data
